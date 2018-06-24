3cec9924b401530f56c91f95c7df8f9731ffb6be
dnikhil135 <dnikhil11@gmail.com>
2018-06-24 15:27:08 +0530

    authentication

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
+/**
+ * @author :
+ * @description :App file it contains the configuration.
+ * @version : 1.0
+ *
+ */
+
 const express = require('express');
 const app = express();
+//Below module is used for logging request
 const morgan = require('morgan');
+//Below module is used for parsing request body
 const bodyParser = require('body-parser');
+require('body-parser-xml')(bodyParser);
+// **Below module is used for connecting with MONGODB.Remove while deploying in AWS.
+
 const moongose = require('mongoose');
 require('body-parser-xml')(bodyParser);
 
 const productsRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 const usersRoutes = require('./api/routes/user');
-const rabbitmqRoutes = require('./api/routes/rabbitmq');
+const rmqproducerRoutes = require('./api/routes/rmqproducer');
+const rmqconsumerRoutes = require('./api/routes/rmqconsumer');
 const xmlparseRoutes = require('./api/routes/xmlparse');
-const recevielogRoutes = require('./api/routes/recevie_log');
-
 
 // For Logging Request on Console.
 app.use(morgan('dev'));
@@ -30,9 +41,10 @@ app.use(bodyParser.xml({
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/users', usersRoutes);
-app.use('/rabbitMQ', rabbitmqRoutes);
 app.use('/xmlparse', xmlparseRoutes);
-app.use('/xmlparse', recevielogRoutes);
+app.use('/rabbitMQ', rmqproducerRoutes);
+app.use('/rmqconsumer', rmqconsumerRoutes);
+
 
 //mongoose.connect('mongodb://nodejs:nodejs@node-shop-api-shard-00-00-5lpcu.mongodb.net:27017,node-shop-api-shard-00-01-5lpcu.mongodb.net:27017,node-shop-api-shard-00-02-5lpcu.mongodb.net:27017/test?ssl=true&replicaSet=node-shop-api-shard-0&authSource=admin&retryWrites=true');
 moongose.connect('mongodb://localhost/noderest', (err) => {
